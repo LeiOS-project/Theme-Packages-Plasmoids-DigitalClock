2a0dc3fc73cce7b17b597ca3d27a1c2ffb3ca42f
Linus Fischer <leicraft@leicraftmc.de>
2026-07-13 11:06:06 +0200

    changed install to clear cache

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ sudo mkdir -p /usr/share/plasma/plasmoids/dev.leios.theme-packages.plasmoids.dig
 sudo rsync -a --delete ./src/ /usr/share/plasma/plasmoids/dev.leios.theme-packages.plasmoids.digitalclock/
 sudo chown -R root:root /usr/share/plasma/plasmoids/dev.leios.theme-packages.plasmoids.digitalclock
 
-systemctl --user restart plasma-plasmashell.service
+#systemctl --user restart plasma-plasmashell.service
+
+systemctl --user stop plasma-plasmashell.service
+
+rm -rf ~/.cache/plasma* ~/.cache/*.kcache
+
+systemctl --user start plasma-plasmashell.service
